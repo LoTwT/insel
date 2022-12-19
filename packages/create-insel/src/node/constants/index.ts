617c7062866b91e240364924e57ea47008635a6c
LoTwT <709937065@qq.com>
2022-12-19 13:33:36 +0800

    feat(create-insel): plugin index html

---
+++ packages/create-insel/src/node/constants/index.ts
@@ -0,0 +1,5 @@
+import { join } from "node:path"
+
+export const PACKAGE_ROOT = join(__dirname, "..", "..", "..")
+
+export const DEFAULT_HTML_PATH = join(PACKAGE_ROOT, "template.html")
--- packages/create-insel/src/node/dev.ts
+++ packages/create-insel/src/node/dev.ts
@@ -1,5 +1,9 @@
 import { createServer } from "vite"
+import { pluginIndexHtml } from "./plugin-insel/indexHtml"
 
 export async function createDevServer(root = process.cwd()) {
-  return createServer({ root })
+  return createServer({
+    root,
+    plugins: [pluginIndexHtml()],
+  })
 }
