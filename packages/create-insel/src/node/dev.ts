e27451dfdc0ede303a2f84ddcb59d69b1031ff9c
LoTwT <709937065@qq.com>
2022-12-20 13:56:05 +0800

    feat(create-insel): add hmr

--- packages/create-insel/src/node/dev.ts
+++ packages/create-insel/src/node/dev.ts
@@ -1,9 +1,10 @@
 import { createServer } from "vite"
 import { pluginIndexHtml } from "./plugin-insel/indexHtml"
+import react from "@vitejs/plugin-react"
 
 export async function createDevServer(root = process.cwd()) {
   return createServer({
     root,
-    plugins: [pluginIndexHtml()],
+    plugins: [pluginIndexHtml(), react()],
   })
 }
