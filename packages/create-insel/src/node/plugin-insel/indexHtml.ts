76236d4ec6a38bd7741dfcd2549779a5a2a968ee
LoTwT <709937065@qq.com>
2022-12-20 14:00:24 +0800

    feat(create-insel): auto inject script in template.html

--- packages/create-insel/src/node/plugin-insel/indexHtml.ts
+++ packages/create-insel/src/node/plugin-insel/indexHtml.ts
@@ -1,11 +1,28 @@
 import { readFile } from "node:fs/promises"
 import { Plugin } from "vite"
-import { DEFAULT_HTML_PATH } from "../constants"
+import { CLIENT_ENTRY_PATH, DEFAULT_HTML_PATH } from "../constants"
 
 export function pluginIndexHtml(): Plugin {
   return {
     name: "insel:index-html",
     apply: "serve",
+
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: "script",
+            attrs: {
+              type: "module",
+              src: `/@fs/${CLIENT_ENTRY_PATH}`,
+            },
+            injectTo: "body",
+          },
+        ],
+      }
+    },
+
     configureServer(server) {
       return () => {
         server.middlewares.use(async (req, res, next) => {
