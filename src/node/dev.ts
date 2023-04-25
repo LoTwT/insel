b8c42b353c121aaa0f60b9a4554c227aa6521f74
LoTwT <709937065@qq.com>
2023-04-25 10:45:30 +0800

    feat: add hmr

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,9 +1,10 @@
 import { createServer as createViteDevServer } from "vite"
 import { pluginIndexHtml } from "./plugin-insel/indexHtml"
+import react from "@vitejs/plugin-react"
 
 export async function createDevServer(root = process.cwd()) {
   return createViteDevServer({
     root,
-    plugins: [pluginIndexHtml()],
+    plugins: [pluginIndexHtml(), react()],
   })
 }
--- src/node/plugin-insel/indexHtml.ts
+++ src/node/plugin-insel/indexHtml.ts
@@ -1,19 +1,43 @@
 import { readFile } from "fs/promises"
 import { Plugin } from "vite"
-import { PACKAGE_ROOT, DEFAULT_HTML_PATH } from "../constants"
+import { DEFAULT_HTML_PATH, CLIENT_ENTRY_PATH } from "../constants"
 
 export function pluginIndexHtml(): Plugin {
   return {
     name: "insel:index-html",
     apply: "serve",
+
+    // insert entry script tag
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: "script",
+            attrs: {
+              type: "module",
+              src: `/@fs/${CLIENT_ENTRY_PATH}`,
+            },
+            injectTo: "body",
+          },
+        ],
+      }
+    },
+
     configureServer(server) {
       return () => {
         server.middlewares.use(async (req, res, next) => {
           // 1. read the content of template.html
-          const html = await readFile(DEFAULT_HTML_PATH, "utf-8")
+          let html = await readFile(DEFAULT_HTML_PATH, "utf-8")
 
           // 2. response HTML browser
           try {
+            html = await server.transformIndexHtml(
+              req.url,
+              html,
+              req.originalUrl,
+            )
+
             res.statusCode = 200
             res.setHeader("Content-Type", "text/html")
             res.end(html)
