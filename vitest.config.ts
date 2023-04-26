dba01b0cecc24fec007242b21a5f06c40b6fb953
LoTwT <709937065@qq.com>
2023-04-26 21:47:13 +0800

    feat: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config"
+import AutoImport from "unplugin-auto-import/vite"
+
+export default defineConfig({
+  plugins: [
+    AutoImport({
+      imports: ["vitest"],
+    }),
+  ],
+  test: {
+    environment: "node",
+    passWithNoTests: true,
+    exclude: ["**/node_modules/**", "**/dist/**"],
+    threads: true,
+  },
+})
