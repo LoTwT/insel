80a01abbaf00464836349c49499dbd2e402d5991
LoTwT <709937065@qq.com>
2023-05-19 17:54:14 +0800

    feat: mdx hmr

--- src/node/plugin-mdx/index.ts
+++ src/node/plugin-mdx/index.ts
@@ -1,6 +1,7 @@
 import { Plugin } from "vite"
 import { pluginMdxRollup } from "./pluginMdxRollup"
+import { pluginMdxHMR } from "./pluginMdxHmr"
 
 export async function pluginMdx(): Promise<Plugin[]> {
-  return [await pluginMdxRollup()]
+  return [await pluginMdxRollup(), pluginMdxHMR()]
 }
