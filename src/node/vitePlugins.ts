544e4586da4c0a4f45354b915c42ce9813cfaa4a
LoTwT <709937065@qq.com>
2023-05-31 14:48:28 +0800

    feat: add unocss

--- src/node/vitePlugins.ts
+++ src/node/vitePlugins.ts
@@ -4,12 +4,15 @@ import pluginReact from "@vitejs/plugin-react"
 import { pluginConfig } from "./plugin-insel/config"
 import { pluginRoutes } from "./plugin-routes"
 import { pluginMdx } from "./plugin-mdx"
+import pluginUnocss from "unocss/vite"
+import unocssOptions from "./unocssOptions"
 
 export async function createVitePlugins(
   config: SiteConfig,
   restartServer?: () => Promise<void>,
   isSSR = false,
 ) {
+  pluginUnocss(unocssOptions)
   return [
     pluginIndexHtml(),
     pluginReact({
--- src/theme-default/layout/index.tsx
+++ src/theme-default/layout/index.tsx
@@ -1,9 +1,12 @@
+import "uno.css"
 import { Content } from "../../runtime/Content"
 
 export function Layout() {
   return (
     <div>
-      <h1>Common Content</h1>
+      <h1 p2 m4 text-red>
+        Common Content
+      </h1>
       <h1>Doc Content</h1>
       <Content />
     </div>
